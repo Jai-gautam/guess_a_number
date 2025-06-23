c0641c1f0ad025098de0c68012c1cb6b19a4c8ab
Jai Gautam <jaigautam22062003@gmail.com>
2025-06-23 18:48:45 +0530

    first change

---
+++ main.py
@@ -0,0 +1,19 @@
+import random
+print("WELCOME TO GUESS THE NUMBER GAME")
+num = random.randint(1,100)
+itr = 0
+check = -1
+while(check == -1):
+
+    guess = int(input("Guess a number"))
+    itr = itr + 1
+    if(guess == num):
+        print(f"You guessed correct number which is {guess}")
+        check+= 1
+    elif(guess > num):
+        print(f"Number is less then {guess}")
+
+    elif(guess < num):
+        print(f"Number is greater then {guess}")    
+
+print(f"your total number of guesses are {itr}")
